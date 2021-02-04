4e8f33b87b452ab7971ef6ab510fc85494deed5c
Randy Whitley <randywhitley84@gmail.com>
2021-02-03 22:31:23 -0600

    updated the routes

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//require(apiRoute)(app);
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
