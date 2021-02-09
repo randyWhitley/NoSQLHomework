4b874b64a178973d4b2fa4612ad783ee417cda5d
Randy Whitley <randywhitley84@gmail.com>
2021-02-08 21:37:15 -0600

    server.js code for altas

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Some Bullshit Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
@@ -10,12 +9,18 @@ const PORT = process.env.PORT || 8080;
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
