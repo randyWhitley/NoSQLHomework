5a437d85aa30bc581a3bb6105374b2352690853a
Randy Whitley <randywhitley84@gmail.com>
2021-02-09 20:27:22 -0600

    added .env config

--- server.js
+++ server.js
@@ -2,28 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const workout = require("./models");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 
-
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
-
+console.log(process.env.MONGODB_URI);
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
 
